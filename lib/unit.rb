c602bea3e87c627bb028b757efeff5d6e68d5a5f
David Vickery <david.vickery.23@gmail.com>
2015-02-10 20:00:37 +0000

    completed assignment

---
+++ lib/unit.rb
@@ -0,0 +1,17 @@
+class Unit
+  attr_accessor :attack_power
+  attr_reader :health_points
+
+  def initialize(health_points = 60, attack_power = 10)
+    @health_points = health_points
+    @attack_power = attack_power   
+  end
+
+  def damage(damage)
+    @health_points -= damage
+  end
+
+  def attack!(enemy)
+    enemy.damage attack_power
+  end
+end
